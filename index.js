ee8d284626bc22f9010bf925881cc648e5b0e479
Dennis Demir <dennisjdemir@gmail.com>
2023-03-14 06:44:22 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(length){
   // Write your code here!
+  if (length <= 400) {
+    return "This one is on me!"
+  } else if (length > 400 && length < 2000) {
+    return "That will be twenty bucks."
+  } else if (length > 2000 && length <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (length > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const message = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return message;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
